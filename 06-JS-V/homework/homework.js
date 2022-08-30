7f18b26b83d1043710becfef63abae51f5163225
ivorojas <ivo.2001@hotmail.com>
2022-08-30 18:23:32 -0300

    asdf

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,6 +8,16 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  function Usuario (usuario, nombre, email, password) {
+    this.usuario = usuario;
+    this.nombre = nombre;
+    this.email = email;
+    this.password = password;
+  }
+  Usuario.prototype.saludar = function () {
+    return `Hola, mi nombre es ${this.nombre}`;
+  }
+  return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
